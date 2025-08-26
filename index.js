165f85143efdc0673381853d356fa309465c4598
ahmad-sq <133887297+ahmad-sq@users.noreply.github.com>
2025-08-27 01:52:43 +0700

    index.js

--- index.js
+++ index.js
@@ -131,6 +131,16 @@ function createBot() {
          if (config.utils['anti-afk'].sneak) {
             bot.setControlState('sneak', true);
          }
+
+         // 🔹 Tambahin gerak maju-mundur tiap 1 menit
+         setInterval(() => {
+            bot.setControlState('forward', true)
+            setTimeout(() => {
+               bot.setControlState('forward', false)
+               bot.setControlState('back', true)
+               setTimeout(() => bot.setControlState('back', false), 1000)
+            }, 1000)
+         }, 60000)
       }
    });
 
